8224786b8f9425373b8a4730364c858a678ea473
RichardsonER22 <151100632+RichardsonER22@users.noreply.github.com>
2023-11-23 19:32:05 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+
+var bandera=false;
+
 function validar (){
+if(bandera){
+    console.log('validacion completa')
+}
+
     alert($("#user").val());
 
 
     var user=document.getElementById("user").value;
     alert(user)
+}
+
+function correctCaptcha(){
+    console.log('Captcha resuelto')
+    bandera=true;
 }
